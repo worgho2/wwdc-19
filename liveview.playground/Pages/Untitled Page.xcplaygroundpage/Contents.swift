7822199bdb5319abbe277f95d581a49a7f797f4e
APPLE DEVELOPER ACADEMY <appledeveloperacademy@10-42-40-41.0101.pcpr.br.iosda.org>
2019-03-22 19:08:09 -0300

    Brunao

--- liveview.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ liveview.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -4,42 +4,41 @@ import SpriteKit
 class GameScene: SKScene {
     
     private var label1 = SKLabelNode()
-    
-    private var canCallSpin: Bool = true
-    private var startSpin: Bool = false
+    private var label2 = SKLabelNode()
     
     override func didMove(to view: SKView) {
-        
+        self.addChild(label1)
         label1.verticalAlignmentMode = .center
-        //starting elements
-        label1.position = CGPoint(x: 256, y: 384)
-        label1.text = "Physics"
-        label1.fontSize = 82
+        label1.position = CGPoint(x: 229, y: 385.0)
+        label1.text = "Spitr"
+        label1.fontSize = 100
         label1.fontColor = UIColor.white
-        label1.fontName = "Lucida console"
-        label1.fontColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        addChild(label1)
+        label1.fontName = "Optima Regular"
+        label1.color = UIColor.black
         
-        backgroundColor = UIColor.black
-    }
-    
-    public func label1Spin() {
-        Timer.scheduledTimer(withTimeInterval: 2.5, repeats: false, block: {(timer) in
-            self.startSpin = true
-        })
+        self.addChild(label2)
+        label2.verticalAlignmentMode = .center
+        label2.position = CGPoint(x: 352, y: 385.0)
+        label2.text = "o"
+        label2.fontSize = 100
+        label2.fontColor = UIColor.white
+        label2.fontName = "Optima Regular"
+        label2.color = UIColor.black
+        
+        self.backgroundColor = UIColor.black
     }
     
     override func update(_ currentTime: TimeInterval) {
-        if self.canCallSpin {
-            self.label1Spin()
-            self.canCallSpin = !self.canCallSpin
-        }
-        if self.startSpin { self.label1.run(SKAction.rotate(byAngle: 2.3, duration: 60)) }
-    
+        self.label2.run(SKAction.rotate(byAngle: 5, duration: 60))
+        self.label2.run(SKAction.colorize(with: UIColor(red: .random(in: 0...1),
+                                                        green: .random(in: 0...1),
+                                                        blue: .random(in: 0...1),
+                                                        alpha: 1.0), colorBlendFactor: 0.4, duration: 0.02))
     }
 }
 
 let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 512, height: 768))
 let scene = GameScene(size: CGSize(width: 512, height: 768))
 sceneView.presentScene(scene)
-PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
+PlaygroundPage.current.liveView = sceneView
+PlaygroundPage.current.needsIndefiniteExecution = true
